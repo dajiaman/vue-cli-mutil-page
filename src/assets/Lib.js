ade111a465892a678999db825470b97afc3aff32
dajiaman <dajiaman@live.com>
2016-11-04 15:48:17 +0800

    first

---
+++ src/assets/Lib.js
@@ -0,0 +1,12 @@
+
+// import 'assets/css.css';
+import C from 'assets/conf';
+import M from 'assets/common';
+import 'radon-ui/dist/static/css/dist.css';
+
+var Rxports = {
+	M,C
+};
+
+module.exports = Rxports
+
